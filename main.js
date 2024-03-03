f826be53e1477aefc82941c8471642a9f23a9915
Sophie Van Schil <sophievanschil@hotmail.com>
2024-03-03 18:50:32 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+import './style.css'
+import Swiper from 'swiper';
+import './script.js'
+
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+var swiper = new Swiper(".mySwiper", {
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
+});
---
+++ script.js
@@ -0,0 +1,24 @@
+import { apiKey } from "./key";
+
+async function fetchWeather(cityName, elementToUpdate) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const temperature = data.main.temp;
+        elementToUpdate.textContent = `${temperature}°C`; // Update temperature display
+    } catch (error) {
+        console.log(`Error fetching weather data for ${cityName}: ${error}`);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const cities = ['Antwerpen', 'Buenos Aires', 'Johannesburg', 'New York', 'Tokyo'];
+    const temperatureElements = document.querySelectorAll('.temperature');
+
+    cities.forEach(async (cityName, index) => {
+        const elementToUpdate = temperatureElements[index];
+        await fetchWeather(cityName, elementToUpdate);
+    });
+});
